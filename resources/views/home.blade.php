bc07a03103ea93c7148d5641e97f37ea2465350c
duonglq <duonglaiquang@gmail.com>
2017-04-21 18:22:27 +0700

    project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,219 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+    <script type="text/javascript" src="/js/bootstrap.min.css"></script>
+    <script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="/js/app.js"></script>
+</head>
+<body>
+    <div id="main">
+         <div id="head">
+            <div id="header">
+                <a href="/" style="color: rgba(212,227,118,0.88)"><b><em>Linh's</em> Weeding Studio</b></a>
+            </div>
+            <div id="head-link">
+                 <ul class="nav navbar-nav">
+                     <li><a href="/">Home</a></li>
+                     <li><a href="/services">Services</a></li>
+                     <li><a href="/about">About</a></li>
+                     <li><a href="/new">New</a></li>
+                     <li><a href="/blog">Blog</a></li>
+                     <li><a href="/contact">Contact</a></li>
+                 </ul>
+             </div>
+         </div>
+
+        <div id="intro">
+            <div id="text">
+                <h4>Welcome to the Linh's Weeding Studio</h4><p>Với phương châm "Trân trọng khoảnh khắc trong tình
+                yêu"- một trong những cách ươm mầm cho hạnh phúc thật bền vững.Linh's Weeding Studio tự hào mang đến
+                bạn sự hoàn thiện hơn trong ngày cưới cũng như giúp bạn lưu trữ những phút giây đáng nhớ nhất ...</p>
+                <a href="#">
+                    <img src="\assets\more.png" style="float: right;margin-right: 20px">
+                </a>
+            </div>
+
+            <div id="picture">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators" >
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox" style="border:solid 10px rgba(225,218,201,0.88)">
+                        <div class="item active">
+                            <img src="\assets\s2.png">
+                            <div class="carousel-caption">
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="\assets\a2.png">
+                            <div class="carousel-caption">
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="\assets\a5.png">
+                            <div class="carousel-caption">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev" style="height: 220px;border-radius: 10px">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next" style="height: 220px;border-radius: 10px">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+         </div>
+
+        <div id="border">
+        </div>
+
+        <div id="head-tag">
+            <div id="service" style="float: left">
+                <img src="\assets\1.png" style="float: left">
+                <h4 style="float:right"><a href="#"><b>Services</b></a></h4>
+            </div>
+
+            <div id="new" style="float: left">
+                <img src="\assets\2.png" style="float: left">
+                <h4 style="float:right"><a href="#"><b>New</b></a></h4>
+            </div>
+
+            <div id="blog" style="float: right">
+                <img src="\assets\3.png" style="float: left">
+                <h4 style="float:right"><a href="#"><b>Blog</b></a></h4>
+            </div>
+
+        </div>
+
+        <div id="main">
+
+         <div id="left">
+            <div id="p1">
+                <h4><a href="#"><b>Costume Designer Weeding</b></a></h4>
+                <p>Màu tình yêu sáng lên trong nắng mới.Mỗi bước chân em như rực sáng trong tà váy
+                trắng tinh khôi và với nụ cười rạng ngời hạnh phúc trên môi,em như thiên sứ bước vào cuộc đời anh.
+                Anh mỉm cười mãn nguyện vì từ nay cuộc đời anh đã có em cùng anh chung bước<br>_ _ _ _ _ _ _ _ _ _ _
+                <a href="#" style="color: #337ab7;float: right">Xem tiếp</a></p>
+            </div>
+
+            <div id="p1">
+                <h4><a href="#"><b>Make Up</b></a></h4>
+                <p>Với phong cách trang điểm và sự kết hợp trang phục đầy tinh tế.Linh's Weeding
+                    Studio luôn thể hiện sự tự nhiên nhẹ nhành nhưng không kém phần sang trọng khuyến rũ...
+                    <br>_ _ _ _ _ _ _ _ _ _ _
+                    <a href="#" style="color: #337ab7;float: right">Xem tiếp</a></p>
+            </div>
+
+            <div id="p1">
+                <h4><a href="#"><b>Photography</b></a></h4>
+                <p>Với những trang thiết bị hiện đại cùng đội ngũ photograper, designer hàng đầu Linh's Weeding
+                    Studio sẽ mang đến cho bạn những trải nghiệm tuyệt với và những khoảng khắc thú vị qua những
+                    picture đẹp nhất cuộc đời bạn...
+                    <br>_ _ _ _ _ _ _ _ _ _ _
+                    <a href="#" style="color: #337ab7;float: right">Xem tiếp</a></p>
+            </div>
+         </div>
+
+         <div id="middle">
+            <div id="p2">
+                <img src="\assets\a5.png" style="width: 500px;height: 200px;margin-top: 10px;border: solid 10px white;
+    border-radius: 10px;outline: solid 5px rgba(225,218,201,0.88);">
+                <h4><img src="\assets\human.png" style="margin-right: 5px"><a href="#"><b>Những bộ áo cưới đẹp nhất năm 1010</b></a></h4>
+                <p>Cùng chiêm ngưỡng những bộ váy cưới đẹp nhất của Hollywood vào năm 2010.<br>_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+                    <a href="#" style="color: #337ab7;float: right">Xem tiếp</a></p>
+            </div>
+
+             <div id="p2">
+                 <img src="\assets\a2.png" style="width: 500px;height: 200px;margin-top: 10px;border: solid 10px white;
+    border-radius: 10px;outline: solid 5px rgba(225,218,201,0.88);">
+                 <h4><img src="\assets\human.png" style="margin-right: 5px"><a href="#"><b>Những cô dâu Hàn xinh đẹp của mùa</b></a></h4>
+                 <p>Với sự tham gia của 6 nữ ca sĩ,diễn viên xinh đẹp trong BST váy cưới 2010 của Void thì
+                     mùa thu này ai sẽ là cô dâu xinh đẹp và nổi bật nhất?<br>_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+                     <a href="#" style="color: #337ab7;float: right">Xem tiếp</a></p>
+             </div>
+
+         </div>
+
+         <div id="right">
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Tâm sự của cô gái chuẩn bị lên xe hoa</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Nhật kí son môi</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Tình yêu và sự lừa dối</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Sẽ có một thiên thần khác thay anh yêu em</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Tình yêu trở lại</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Love is never die</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Hoài niệm</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Khi yêu đừng quay đầu lại</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Ai cho tôi niềm tin</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Chia tay</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Đừng bắt em phải quên anh</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Yêu là cưới</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Mưa nước mắt</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Hãy để anh làm bờ vai để em tựa vào</a></h5>
+             <h5><img src="\assets\tick.png" style="margin-right: 5px"><a href="#">Cưới người không yêu</a></h5>
+
+         </div>
+
+        </div>
+
+        <div id="footer">
+            <div id="footer-left">
+                <div style="width: 100%;height:20px">
+                    <img src="\assets\icon.png" style="width: 150px;height: 20px;margin-top: 20px;margin-left: 50px">
+                </div>
+
+                <div id="box">
+                     <div id="b1">
+                        <a href="/"><h4>Home</h4></a>
+                     </div>
+                     <div id="b1">
+                        <a href="/services"><h4>Services</h4></a>
+                     </div>
+                </div>
+
+                <div id="box">
+                     <div id="b1">
+                        <a href="/about"><h4>About</h4></a>
+                      </div>
+                    <div id="b1">
+                        <a href="/new"><h4>New</h4></a>
+                    </div>
+                 </div>
+
+                <div id="box">
+                    <div id="b1">
+                         <a href="/blog"><h4>Blog</h4></a>
+                      </div>
+                    <div id="b1">
+                         <a href="/contact"><h4>Contact</h4></a>
+                    </div>
+                </div>
+
+            </div>
+            <div id="footer-right">
+                <h5 style="color: white;margin-top: 20px;margin-left: 50px">Add: 264 Đội Cấn,Ba Đình,Hà Nội-Phone: 04.38388818</h5>
+                <h5 style="color: white;margin-top: 1px;margin-left: 50px">Tel: 0985891426 &emsp;&emsp;&emsp;         Email ljnh.cool@yahoo.com.vn</h5>
+                <h5 style="color:white;float:right;margin-top: 50px;margin-right: 50px">Copyright &copy; 2010 Linh's Weeding Studio. All rights reserved</h5>
+             </div>
+        </div>
+</div>
+
+</body>
+</html>
